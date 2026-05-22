ef497c91c96c126cc37f4a2733d186d288e5dfa4
Tim Hahn <hahntj@gmail.com>
2026-05-22 17:16:28 -0400

    feat: use NPM-served @ncfour-us/projen-utils package

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { TypeScriptESMProject } from '@ncfour/projen-utils';
+import { TypeScriptESMProject } from '@ncfour-us/projen-utils';
 import { javascript } from 'projen';
 
 const project = new TypeScriptESMProject({
@@ -8,10 +8,10 @@ const project = new TypeScriptESMProject({
   name: 'logging',
   projenrcTs: true,
   packageManager: javascript.NodePackageManager.PNPM,
-  repository: 'https://github.com/hahntj/ncfour/logging.git',
+  repository: 'https://github.com/ncfour-us/logging.git',
 
   // set up the package name in package.json
-  packageName: '@ncfour/logging',
+  packageName: '@ncfour-us/logging',
 
   // set up the project with a LICENSE and copyright info
   license: 'MIT',
@@ -24,10 +24,7 @@ const project = new TypeScriptESMProject({
     prettier: true,
   },
 
-  devDeps: [
-    '@ncfour/projen-utils@file:/home/tjh/Projects/repos/ncfour/projen-utils/dist/js/projen-utils@0.0.0.jsii.tgz',
-    '@jest/globals',
-  ],
+  devDeps: ['@ncfour-us/projen-utils', '@jest/globals'],
 
   /* Runtime dependencies of this module. */
   deps: ['chalk', 'winston'],
